87640143c9567528a967bad4c117efabe49c8e95
Patrick Mealey <patrick.d.mealey@gmail.com>
2022-03-13 23:43:06 -0400

    Fixed heardle parser

--- backend/Services/Parsers/HeardleParser.cs
+++ backend/Services/Parsers/HeardleParser.cs
@@ -16,7 +16,7 @@ namespace backend.Services.Parsers
         public override int Priority => _priority;
         private const string _gameName = "Heardle";
         public override string GameName => _gameName;
-        private readonly Regex _parser = new Regex($"#{_gameName} #\\d+[\\s\\r\\n]+🔉(?<{ScoreGroup}>[🟥🟩]+)");
+        private readonly Regex _parser = new Regex($"#{_gameName} #\\d+[\\s\\S]*?[🔈🔉🔊](?<{ScoreGroup}>[🟥🟩]+)");
         protected override Regex Parser => _parser;
         protected override string? ExtraContent => _url;
         private const string _url = "https://heardle.app";
