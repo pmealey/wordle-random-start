b168ce8cc0201b701383f82a94abae63f9f485a6
Patrick Mealey <patrick.d.mealey@gmail.com>
2022-03-22 19:28:56 -0400

    Updated globle parser

--- backend/Services/Parsers/GlobleParser.cs
+++ backend/Services/Parsers/GlobleParser.cs
@@ -15,9 +15,9 @@ namespace backend.Services.Parsers
         public override int Priority => _priority;
         private const string _gameName = "Globle";
         public override string GameName => _gameName;
-        private readonly Regex _parser = new Regex($"🌎.*?🌍[\\s\n\r]+Today's guesses: (?<{ScoreGroup}>\\d)");
+        private readonly Regex _parser = new Regex($"🌎.*?🌍[\\s\n\r]+[^=]*= (?<{ScoreGroup}>\\d+)");
         protected override Regex Parser => _parser;
-        protected override string? ExtraContent => _url;
+        protected override string? ExtraContent => "#globle";
         private const string _url = "https://globle-game.com";
         public override string Url => _url;
     }
