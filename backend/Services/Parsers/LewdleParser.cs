7993d8b54e5826e461ba82fdbfbabf7509bf17c8
Patrick Mealey <patrick.d.mealey@gmail.com>
2022-05-01 10:29:43 -0400

    Fixed lewdle url

--- backend/Services/Parsers/LewdleParser.cs
+++ backend/Services/Parsers/LewdleParser.cs
@@ -17,8 +17,8 @@ namespace backend.Services.Parsers
         public override string GameName => _gameName;
         private readonly Regex _parser = new Regex($"{_gameName}.*?(?<{ScoreGroup}>[\\d|X])/\\d");
         protected override Regex Parser => _parser;
-        protected override string? ExtraContent => null;
-        private const string _url = "lewdlegame.com";
+        protected override string? ExtraContent => "lewdlegame.com";
+        private const string _url = "https://www.lewdlegame.com";
         public override string Url => _url;
     }
 }
