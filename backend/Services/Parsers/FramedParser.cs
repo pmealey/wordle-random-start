b438c7a274bc313c3b556601a2d1254e083f7f13
Patrick Mealey <patrick.d.mealey@gmail.com>
2022-05-06 17:34:00 -0400

    Added the framed parser

---
+++ backend/Services/Parsers/FramedParser.cs
@@ -0,0 +1,49 @@
+using System.Text.RegularExpressions;
+using backend.Models;
+
+namespace backend.Services.Parsers
+{
+    public class FramedParser : BasicScoreResultParser
+    {
+        private ILogger<FramedParser> _logger;
+
+        public FramedParser(ILogger<FramedParser> logger) : base(logger)
+        {
+            _logger = logger;
+        }
+
+        private const int _priority = 10;
+        public override int Priority => _priority;
+        private const string _gameName = "Framed";
+        public override string GameName => _gameName;
+        // \uD83C\uDFA5 = 🎥
+        // \uD83D\uDFE9 = 🟩
+        // \uD83D\uDFE8 = 🟨
+        // \uD83D\uDFE5 = 🟥
+        // \u2B1C\uFE0F = ⬜️
+        // \u2B1B\uFE0F = ⬛️
+        private readonly Regex _parser = new Regex($"{_gameName} #\\d+\n\uD83C\uDFA5(?<{ScoreGroup}>[^\n]*)", RegexOptions.Singleline);
+        protected override Regex Parser => _parser;
+        protected override string? ExtraContent => _url;
+        private const string _url = "https://framed.wtf";
+        public override string Url => _url;
+
+        protected override DailyResult SetScore(DailyResult dailyResult, Match parserResults)
+        {
+            if (!parserResults.Groups.ContainsKey(ScoreGroup))
+            {
+                return dailyResult;
+            }
+
+            var score = parserResults.Groups[ScoreGroup].Value.Trim().Replace(" ", string.Empty);
+
+            var successIndex = score.IndexOf("🟩");
+            if (successIndex > -1)
+            {
+                dailyResult.Score = (successIndex + "🟩".Length) / 2;
+            }
+
+            return dailyResult;
+        }
+    }
+}
