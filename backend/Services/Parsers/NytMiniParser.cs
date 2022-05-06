b2319eaacda05987eea28bcc97108edec036980c
Patrick Mealey <patrick.d.mealey@gmail.com>
2022-05-06 17:16:27 -0400

    Added NYT Mini URL

--- backend/Services/Parsers/NytMiniParser.cs
+++ backend/Services/Parsers/NytMiniParser.cs
@@ -12,11 +12,12 @@ namespace backend.Services.Parsers
         }
 
         private const int _priority = 5;
-        public override int Priority => _priority;        private const string _gameName = "NYT Mini";
+        public override int Priority => _priority;
+        private const string _gameName = "NYT Mini";
         public override string GameName => _gameName;
         private readonly Regex _parser = new Regex($"NYTM (?<{TimeGroup}>[:\\d]+)", RegexOptions.IgnoreCase);
         protected override Regex Parser => _parser;
-        private const string _url = null;
+        private const string _url = "https://www.nytimes.com/crosswords/game/mini";
         public override string Url => _url;
     }
 }
