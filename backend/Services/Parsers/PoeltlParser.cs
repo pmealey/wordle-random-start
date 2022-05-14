0fefe4caf162446c6d00866b8d35163984fd9266
Patrick Mealey <patrick.d.mealey@gmail.com>
2022-05-13 21:59:51 -0400

    Fixed Poeltl parser

--- backend/Services/Parsers/PoeltlParser.cs
+++ backend/Services/Parsers/PoeltlParser.cs
@@ -13,7 +13,7 @@ namespace backend.Services.Parsers
 
         private const string _gameName = "Poeltl";
         public override string GameName => _gameName;
-        private readonly Regex _parser = new Regex($"#{_gameName} \\d+ - (?<{ScoreGroup}>[\\d|X])/\\d");
+        private readonly Regex _parser = new Regex($"{_gameName} \\d+ - (?<{ScoreGroup}>[\\d|X])/\\d");
         protected override Regex Parser => _parser;
         protected override string? ExtraContent => null;
         private const string _url = "https://poeltl.dunk.town/";
