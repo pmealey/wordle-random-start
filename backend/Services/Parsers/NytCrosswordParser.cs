e2152c627b4c1da061a3f6ca65b7ed91c13e9813
Patrick Mealey <patrick.d.mealey@gmail.com>
2022-05-15 21:57:00 -0400

    Fixed new . handling for NYT results

--- backend/Services/Parsers/NytCrosswordParser.cs
+++ backend/Services/Parsers/NytCrosswordParser.cs
@@ -15,7 +15,7 @@ namespace backend.Services.Parsers
         public override bool CountWinner => false;
         public override string GameName => "NYT Crossword";
         public override string? HelpText => "Enter \"Nytc 42:42\", \"Nytc 42.42\", or \"Nytc 1:42:42\" for example.";
-        protected override Regex Parser => new Regex($"NYTC (?<{TimeGroup}>[:\\d]+)", RegexOptions.IgnoreCase);
+        protected override Regex Parser => new Regex($"NYTC (?<{TimeGroup}>[:\\d\\.]+)", RegexOptions.IgnoreCase);
         public override string? Url => null;
     }
 }
