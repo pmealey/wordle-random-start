76167bd393cb7d36cae6fa85dac6e4056c1f9b37
Patrick Mealey <patrick.d.mealey@gmail.com>
2023-05-07 09:05:11 -0400

    Updated the NYT Mini parser to accept the official 'share' format

--- backend/Services/Parsers/NytMiniParser.cs
+++ backend/Services/Parsers/NytMiniParser.cs
@@ -14,7 +14,7 @@ namespace backend.Services.Parsers
         public override string Category => "Word";
         public override string GameName => "NYT Mini";
         public override string? HelpText => "Enter \"Nytm 42\", \"Nytm 1:42\", or \"Nytm 1.42\" for example.";
-        protected override Regex Parser => new Regex($"NYTM (?<{TimeGroup}>[:\\d\\.]+)", RegexOptions.IgnoreCase);
-        public override string Url => "https://www.nytimes.com/crosswords/game/mini";
+        protected override Regex Parser => new Regex($"(I solved the [\\d/]+ New York Times Mini Crossword in|NYTM) (?<{TimeGroup}>[:\\d\\.]+)!?", RegexOptions.IgnoreCase);
+        public override string Url => "https://fpx3r.app.goo.gl/PKC4";
     }
 }
