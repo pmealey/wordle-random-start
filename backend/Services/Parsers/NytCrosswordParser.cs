c3aae1a46f2a3c0ad2c08038834bdc4a9672135a
Patrick Mealey <patrick.d.mealey@gmail.com>
2023-05-07 09:04:51 -0400

    Updated the NYT Crossword parser to accept the official 'share' format

--- backend/Services/Parsers/NytCrosswordParser.cs
+++ backend/Services/Parsers/NytCrosswordParser.cs
@@ -15,7 +15,7 @@ namespace backend.Services.Parsers
         public override bool CountWinner => false;
         public override string GameName => "NYT Crossword";
         public override string? HelpText => "Enter \"Nytc 42:42\", \"Nytc 42.42\", or \"Nytc 1:42:42\" for example.";
-        protected override Regex Parser => new Regex($"NYTC (?<{TimeGroup}>[:\\d\\.]+)", RegexOptions.IgnoreCase);
+        protected override Regex Parser => new Regex($"(I solved the (Monday|Tuesday|Wednesday|Thursday|Saturday|Sunday) [\\d/]+ New York Times Daily Crossword in|NYTC) (?<{TimeGroup}>[:\\d\\.]+)", RegexOptions.IgnoreCase);
         public override string? Url => null;
     }
 }
