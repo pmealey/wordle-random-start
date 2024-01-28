49cb918dc492a890dcd44ff10f5c1d359446ad46
Patrick Mealey <patrick.d.mealey@gmail.com>
2024-01-28 14:29:29 -0500

    Added the HertlParser

--- backend/Program.cs
+++ backend/Program.cs
@@ -50,6 +50,7 @@ builder.Services.AddScoped<ResultParser, ArtleParser>();
 builder.Services.AddScoped<ResultParser, NerdleParser>();
 builder.Services.AddScoped<ResultParser, SubwaydleParser>();
 builder.Services.AddScoped<ResultParser, PoeltlParser>();
+builder.Services.AddScoped<ResultParser, HertlParser>();
 builder.Services.AddScoped<ResultParser, SedecordleParser>();
 builder.Services.AddScoped<ResultParser, SedecOrderParser>();
 builder.Services.AddScoped<ResultParser, SedecordleSaviorParser>();
---
+++ backend/Services/Parsers/HertlParser.cs
@@ -0,0 +1,22 @@
+using System.Text.RegularExpressions;
+
+namespace backend.Services.Parsers
+{
+    public class HertlParser : BasicScoreResultParser
+    {
+        private ILogger<HertlParser> _logger;
+
+        public HertlParser(ILogger<HertlParser> logger) : base(logger)
+        {
+            _logger = logger;
+        }
+
+        public override bool CountWinner => false;
+        public override bool Default => false;
+        public override string GameName => "Hertl";
+        public override string? HelpText => null;
+        protected override Regex Parser => new Regex($@"HERTL \(Game #\d+\) - (?<{ScoreGroup}>[\d|X]) / \d");
+        protected override string? ExtraContent => Url;
+        public override string Url => "https://www.hertl.app";
+    }
+}
