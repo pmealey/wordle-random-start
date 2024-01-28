d39354baaddfa9ca19742633a339f07b4eae0e31
Patrick Mealey <patrick.d.mealey@gmail.com>
2024-01-28 14:21:07 -0500

    Added the SedecOrderParser

--- backend/Program.cs
+++ backend/Program.cs
@@ -51,6 +51,7 @@ builder.Services.AddScoped<ResultParser, NerdleParser>();
 builder.Services.AddScoped<ResultParser, SubwaydleParser>();
 builder.Services.AddScoped<ResultParser, PoeltlParser>();
 builder.Services.AddScoped<ResultParser, SedecordleParser>();
+builder.Services.AddScoped<ResultParser, SedecOrderParser>();
 builder.Services.AddScoped<ResultParser, PimantleParser>();
 builder.Services.AddScoped<ResultParser, SemantleParser>();
 builder.Services.AddScoped<ResultParser, RedactleParser>();
---
+++ backend/Services/Parsers/SedecOrderParser.cs
@@ -0,0 +1,41 @@
+using System.Text.RegularExpressions;
+using backend.Models;
+
+namespace backend.Services.Parsers
+{
+    public class SedecOrderParser : BasicScoreResultParser
+    {
+        private ILogger<SedecOrderParser> _logger;
+
+        public SedecOrderParser(ILogger<SedecOrderParser> logger) : base(logger)
+        {
+            _logger = logger;
+        }
+
+        public override bool CountWinner => false;
+        public override bool Default => false;
+        public override string GameName => "Sedec-order";
+        public override string? HelpText => null;
+        protected override Regex Parser => new Regex($@"Daily {GameName} #\d+\nGuesses: (?<{ScoreGroup}>(\d\d?)|X).*?{ExtraContent}", RegexOptions.Singleline);
+        protected override string? ExtraContent => "sedecordle.com\n#sedecordle  #sedecorder";
+        public override string Url => "https://www.sedecordle.com/sedec-order";
+
+        public override string? GetScoreValue(DailyResult dailyResult)
+        {
+            if (dailyResult.Scores == null)
+            {
+                return null;
+            }
+
+            return "\"" + string.Join(",", Enumerable.Range(0, 16).ToArray()
+                .Select((i) => {
+                    if (dailyResult.Scores.Count <= i)
+                    {
+                        return "X";
+                    }
+
+                    return dailyResult.Scores[i].ToString();
+                })) + "\"";
+        }
+    }
+}
