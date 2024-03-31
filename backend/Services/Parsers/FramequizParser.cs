795f73a563237831ea993eff3c6d981381261af7
Patrick Mealey <patrick.d.mealey@gmail.com>
2024-03-31 11:25:50 -0400

    Added the Framequiz parser

---
+++ backend/Services/Parsers/FramequizParser.cs
@@ -0,0 +1,44 @@
+using System.Text.RegularExpressions;
+using backend.Models;
+
+namespace backend.Services.Parsers
+{
+    public class FramequizParser : BasicScoreResultParser
+    {
+        private ILogger<FramequizParser> _logger;
+
+        public FramequizParser(ILogger<FramequizParser> logger) : base(logger)
+        {
+            _logger = logger;
+        }
+
+        public override bool CountWinner => false;
+        public override bool Default => false;
+        public override string GameName => "Framequiz";
+        public override string? HelpText => null;
+        // \uD83D\uDFE9 = 🟩
+        // \uD83D\uDFE5 = 🟥
+        // \u2B1B\uFE0F = ⬛️
+        protected override Regex Parser => new Regex($"{GameName} .+?(?<{ScoreGroup}>[\uD83D\uDFE9\uDFE5\u2B1B\uFE0F ]{{2,}})", RegexOptions.Singleline);
+        protected override string? ExtraContent => $" - {Url}";
+        public override string Url => "https://framequiz.com";
+
+        protected override DailyResult SetScore(DailyResult dailyResult, Match parserResults)
+        {
+            if (!parserResults.Groups.ContainsKey(ScoreGroup))
+            {
+                return dailyResult;
+            }
+
+            var score = parserResults.Groups[ScoreGroup].Value.Trim().Replace(" ", string.Empty);
+
+            var successIndex = score.IndexOf("🟩");
+            if (successIndex > -1)
+            {
+                dailyResult.Score = (successIndex + "🟩".Length) / 2;
+            }
+
+            return dailyResult;
+        }
+    }
+}
