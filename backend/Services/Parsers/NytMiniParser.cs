b1e85a2f0f50dd69fffbffbf384a07e2fda5c35a
Patrick Mealey <patrick.d.mealey@gmail.com>
2024-05-25 12:22:38 -0400

    Fixed the NYT Mini URL

--- backend/Services/Parsers/NytMiniParser.cs
+++ backend/Services/Parsers/NytMiniParser.cs
@@ -15,6 +15,6 @@ namespace backend.Services.Parsers
         public override string GameName => "NYT Mini";
         public override string? HelpText => "Alternate entry: \"Nytm 42\", \"Nytm 1:42\", or \"Nytm 1.42\".";
         protected override Regex Parser => new Regex($"(I solved the [\\d/]+ New York Times Mini Crossword in|NYTM) (?<{TimeGroup}>[:\\d\\.]+)!?", RegexOptions.IgnoreCase);
-        public override string Url => "https://fpx3r.app.goo.gl/PKC4";
+        public override string Url => "https://www.nytimes.com/crosswords/game/mini";
     }
 }
