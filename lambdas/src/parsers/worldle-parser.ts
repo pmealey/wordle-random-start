a84aaf6c5a7a2c7ef823ee78780fa50ab4ce024b
Patrick Mealey <patrick.d.mealey@gmail.com>
2026-02-04 20:59:47 -0500

    Fix worldle parser

--- lambdas/src/parsers/worldle-parser.ts
+++ lambdas/src/parsers/worldle-parser.ts
@@ -4,8 +4,8 @@ export class WorldleParser extends BasicScoreResultParser {
   readonly countWinner = true;
   readonly gameName = 'Worldle';
   readonly helpText = null;
-  protected readonly extraContent = `${this.url}/share`;
   readonly url = 'https://worldle.teuteuf.fr';
+  protected readonly extraContent = `${this.url}/share`;
   
   // Matches "#Worldle #1471 (31.01.2026) X/6" or "#Worldle #44 1/6"
   // Note: C# uses [\d|X] which incorrectly includes literal pipe - fixed here to [\dX]
